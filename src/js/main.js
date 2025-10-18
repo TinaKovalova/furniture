0e725bb060364471cd80e357729b4d56a625042a
TinaKovalova <kovalevatkv@gmail.com>
2025-10-18 23:26:00 +0300

    reviews

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
 import "/src/scss/style.scss";
+
 
 window.addEventListener("load", () => {
 
@@ -8,6 +12,25 @@ window.addEventListener("load", () => {
   burgerButton?.addEventListener("click", (e) => {
     header?.classList.toggle("_menu-open")
   })
+
+  const swiper = new Swiper(".reviews__slider.swiper", {
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    modules: [Navigation],
+    navigation: {
+      nextEl: ".reviews__slider-button.reviews__slider-button--next",
+      prevEl: ".reviews__slider-button.reviews__slider-button--prev",
+    },
+
+    breakpoints: {
+      992: {
+        spaceBetween: 57,
+      },
+    },
+  });
+
+
 
 });
 
