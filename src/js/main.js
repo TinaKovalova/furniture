322c43a1c025454efe505ec0dcea5c671bf69a96
TinaKovalova <kovalevatkv@gmail.com>
2025-10-19 19:15:18 +0300

    header hide

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,30 @@ import "/src/scss/style.scss";
 window.addEventListener("load", () => {
 
   const burgerButton = document.querySelector(".burger-button");
-  const header = document.querySelector(".header")
+  const header = document.querySelector(".header");
+  const headerMenu = document.querySelector(".header__menu");
   
   burgerButton?.addEventListener("click", (e) => {
     header?.classList.toggle("_menu-open")
   })
+  headerMenu?.addEventListener("click", (e) => {
+    if (e.target.tagName === "A" && header?.classList.contains("_menu-open")) {
+     header.classList.remove("_menu-open");
+    }
+  })
+
+  let lastScroll = 0;
+  const isContainClassHide = () => header.classList.contains("hide");
+  window.addEventListener("scroll", () => {
+  let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+  if (scrollPosition > lastScroll && scrollPosition > header.offsetHeight && !isContainClassHide()) {
+    header.classList.add("hide");
+  } else if (scrollPosition < lastScroll && isContainClassHide()) {
+    header.classList.remove("hide");
+  }
+  lastScroll = scrollPosition;
+});
+
 
   const swiper = new Swiper(".reviews__slider.swiper", {
     loop: true,
@@ -22,7 +41,6 @@ window.addEventListener("load", () => {
       nextEl: ".reviews__slider-button.reviews__slider-button--next",
       prevEl: ".reviews__slider-button.reviews__slider-button--prev",
     },
-
     breakpoints: {
       992: {
         spaceBetween: 57,
